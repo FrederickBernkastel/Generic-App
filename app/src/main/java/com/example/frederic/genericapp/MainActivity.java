ae89af9f3c6855511c1675d18bbd12b36769f763
FrederickBernkastel <brian_chew_ee_da@hotmail.com>
2018-02-15 09:44:42 +0800

    Added main activity layout without functionality Created empty DatabaseConnector class without functionality
    Signed-off-by: FrederickBernkastel <brian_chew_ee_da@hotmail.com>

---
+++ app/src/main/java/com/example/frederic/genericapp/MainActivity.java
@@ -0,0 +1,39 @@
+/*
+    Main Menu Activity
+*/
+
+package com.example.frederic.genericapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get screen size
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        // Set Guest Button params dynamically
+        Button guestButton = findViewById(R.id.guest_button);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(0, height/10, 0, height/10);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        guestButton.setLayoutParams(params);
+
+    }
+}
