d05437ca66a827f330bca9724c10a70e0654c1f5
FrederickBernkastel <brian_chew_ee_da@hotmail.com>
2018-03-08 19:07:48 +0800

    Edited DatabaseConnector to connect to server successfully

--- app/src/main/java/com/example/frederic/genericapp/DatabaseConnector.java
+++ app/src/main/java/com/example/frederic/genericapp/DatabaseConnector.java
@@ -5,7 +5,6 @@ package com.example.frederic.genericapp;
 
 
 import android.os.AsyncTask;
-import android.util.JsonReader;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -15,16 +14,9 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.SocketAddress;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.stream.Collectors;
-
-import javax.net.ssl.HttpsURLConnection;
-
-
+import java.net.URLConnection;
 
 
 /*
@@ -34,10 +26,8 @@ import javax.net.ssl.HttpsURLConnection;
 */
 class DatabaseConnector {
     //TODO: change url
-    private static final String SERVERURLSTRING = "www.INSERTURL.com";
+    private static final String SERVERURLSTRING = "http://10.12.79.231:4995/api/menu";
     private static final String CHARSET = "UTF-8";
-    
-    
 
     // Parse JSON file, and output relevant class, class should have an ID identifier
     static RestaurantMenu parseJSON(String s){
@@ -80,7 +70,7 @@ class DatabaseConnector {
         try {
 
             JSONObject restaurantJSON = new JSONObject(s);
-                    String restaurantName = restaurantJSON.getString("name");
+            String restaurantName = restaurantJSON.getString("name");
             String url = restaurantJSON.getString("imagehyperlink");
             RestaurantMenu menu = new RestaurantMenu(restaurantName,url);
             JSONArray menuItems = restaurantJSON.getJSONArray("menu");
@@ -100,13 +90,17 @@ class DatabaseConnector {
 
     }
 
-    private class FetchTaskInput{
+    // Input information for fetching data
+    static class FetchTaskInput{
+        FetchTaskInput(String paylahID){
+            this.paylahID = paylahID;
+        }
         String paylahID;
 
     }
 
-    // AsyncTask to fetch JSON code from database
-    private static class FetchTask extends AsyncTask<FetchTaskInput, Void, FetchedObject> {
+    // AsyncTask to fetch a restautant's JSON code from database
+    static class FetchTask extends AsyncTask<FetchTaskInput, Void, FetchedObject> {
         @Override
         protected FetchedObject doInBackground(FetchTaskInput... params) {
             FetchTaskInput FetchTaskInput = params[0];
@@ -116,7 +110,8 @@ class DatabaseConnector {
                 // Create URL
                 URL serverURL = new URL(SERVERURLSTRING);
                 // Create connection
-                HttpsURLConnection myConnection = (HttpsURLConnection) serverURL.openConnection();
+                //HttpsURLConnection myConnection = (HttpsURLConnection) serverURL.openConnection();
+                HttpURLConnection myConnection = (HttpURLConnection) serverURL.openConnection();
 
                 // TODO: Set request Headers
                 myConnection.setRequestProperty("User-Agent", paylahID);
